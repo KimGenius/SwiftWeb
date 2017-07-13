80d4f2a5700ef04523f9bb75a53bc08723f82ecb
kim-youngjae <youngjae1047@naver.com>
2017-07-13 21:35:55 +0900

    Edit Main.storyboard & ViewController.swift

--- SwiftWeb/ViewController.swift
+++ SwiftWeb/ViewController.swift
@@ -5,23 +5,56 @@
 //  Created by 김영재 on 2017. 7. 13..
 //  Copyright (c) 2017 김영재. All rights reserved.
 //
+//
+//  ViewController.swift
+//  SwiftWeb
+//
+//  Created by 김영재 on 2017. 7. 13..
+//  Copyright © 2017년 김영재. All rights reserved.
+//
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var txtUrl: UITextField!
+    @IBOutlet weak var myWebView: UIWebView!
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url:String) {
+
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
-
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
 }
+
