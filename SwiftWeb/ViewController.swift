3f7b86411be9624a895a028398ef929ed4cd07dd
김영재 <kim-youngjae@gim-yeongjaeui-MacBook-Air.local>
2017-07-13 21:07:21 +0900

    Initial commit

---
+++ SwiftWeb/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftWeb
+//
+//  Created by 김영재 on 2017. 7. 13..
+//  Copyright (c) 2017 김영재. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
---
+++ SwiftWebTests/SwiftWebTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftWebTests.swift
+//  SwiftWebTests
+//
+//  Created by 김영재 on 2017. 7. 13..
+//  Copyright (c) 2017 김영재. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftWeb
+
+class SwiftWebTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
